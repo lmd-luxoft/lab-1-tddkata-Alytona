feaf479c48bec306dde45c49772c1ca5cf03c2f7
92VoronchikhinAE <VoronchikhinAE01@cbr.ru>
2021-05-11 13:16:33 +0300

    Тесты, первый вариант.

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -16,5 +16,87 @@ namespace TDDKata
             int value = calc.Sum("2,2");
             Assert.That(value, Is.EqualTo(4), "Wrong actual value");
         }
+
+        [Test]
+        public void SumNull ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( null );
+
+            Assert.That( value, Is.EqualTo( -1 ), "Wrong actual value" );
+        }
+        [Test]
+        public void SumEmpty ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( "" );
+
+            Assert.That( value, Is.EqualTo( -1 ), "Wrong actual value" );
+        }
+
+        [Test]
+        public void SumOneCorrect ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( "1" );
+
+            Assert.That( value, Is.EqualTo( 1 ), "Wrong actual value" );
+        }
+        [Test]
+        public void SumOneNegative ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( "-5" );
+
+            Assert.That( value, Is.EqualTo( -1 ), "Wrong actual value" );
+        }
+        [Test]
+        public void SumOneNoNumber ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( "ABC" );
+
+            Assert.That( value, Is.EqualTo( -1 ), "Wrong actual value" );
+        }
+        [Test]
+        public void SumTwoCorrect ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( "1,2" );
+
+            Assert.That( value, Is.EqualTo( 3 ), "Wrong actual value" );
+        }
+        [Test]
+        public void SumTwoCorrectManyDigits ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( "12323,24444" );
+
+            Assert.That( value, Is.EqualTo( 12323 + 24444 ), "Wrong actual value" );
+        }
+        [Test]
+        public void SumTwoIncorrectDelimiter ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( "1 2" );
+
+            Assert.That( value, Is.EqualTo( 3 ), "Wrong actual value" );
+        }
+        [Test]
+        public void SumTwoFirstNegative ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( "-1,2" );
+
+            Assert.That( value, Is.EqualTo( -1 ), "Wrong actual value" );
+        }
+        [Test]
+        public void SumTwoSecondNegative ()
+        {
+            StringCalc calc = new StringCalc();
+            int value = calc.Sum( "1,-2" );
+
+            Assert.That( value, Is.EqualTo( -1 ), "Wrong actual value" );
+        }
     }
 }
