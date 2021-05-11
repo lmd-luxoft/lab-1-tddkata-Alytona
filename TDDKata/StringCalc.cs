f05beefd49b311c2d57ac986b35a49563c7704d7
92VoronchikhinAE <VoronchikhinAE01@cbr.ru>
2021-05-11 13:32:05 +0300

    Реализация.

--- TDDKata/StringCalc.cs
+++ TDDKata/StringCalc.cs
@@ -6,9 +6,34 @@ namespace TDDKata
 {
     internal class StringCalc
     {
+        char[] _delimiters = new char[] { ',', ' ' };
+
         internal int Sum(string v)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+
+            if (v == null)
+                return -1;
+
+            if (v.Trim().Length == 0)
+                return 0;
+
+            string[] numbers = v.Split( _delimiters );
+            if (numbers.Length > 2)
+                return -1;
+
+            foreach (string number in numbers) 
+            {
+                int numberValue = -1;
+                if (!Int32.TryParse( number, out numberValue ))
+                    return -1;
+
+                if (numberValue < 0)
+                    return -1;
+
+                sum += numberValue;
+            }
+            return sum;
         }
     }
 }
